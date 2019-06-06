b7b281c2cdc1824ca5dfbc8b7d632da1525dbf06
Joshua Anthony <jantho1990@gmail.com>
2019-06-06 18:24:58 -0500

    added a tilemap; made Vec1 have collision similar to Gecko

--- Vec1.gd
+++ Vec1.gd
@@ -5,6 +5,7 @@ extends KinematicBody2D
 # var b = "text"
 
 const SPEED = 200
+const THRESHOLD = 7
 
 var motion = Vector2(0, 0)
 
@@ -17,17 +18,21 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	var speed = SPEED
+	if Input.is_action_pressed("slow"):
+		speed *= 0.1
+	
 	if Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = -speed
 	elif Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = speed
 	else:
 		motion.x = 0
 	
 	if Input.is_action_pressed("ui_up"):
-		motion.y = -SPEED
+		motion.y = -speed
 	elif Input.is_action_pressed("ui_down"):
-		motion.y = SPEED
+		motion.y = speed
 	else:
 		motion.y = 0
 	
@@ -39,10 +44,13 @@ func _physics_process(delta):
 	calculate_grippable_surfaces()
 	motion = move_and_slide(motion)
 	
+	if Input.is_action_just_pressed("reset_rotation"):
+		reset_rotation()
+	
 	update()
 
 func _draw():
-	draw_circle(Vector2(0, 0), get_half_width(), Color(1, 0, 0))
+	draw_rect(Rect2(0 - get_half_width(), 0 - get_half_height(), get_width(), get_height()), Color(1, 1, 1))
 	
 	draw_grip_range()
 
@@ -90,17 +98,17 @@ func calculate_grippable_surfaces(pos = position):
 	
 	grippable_surface = ret
 
-func get_half_width():
-	return 12
+func get_height():
+	return $CollisionShape2D.shape.extents.y * 2
 
 func get_half_height():
-	return 16
+	return $CollisionShape2D.shape.extents.y
 
 func get_width():
-	return 24
+	return $CollisionShape2D.shape.extents.x * 2
 
-func get_height():
-	return 32
+func get_half_width():
+	return $CollisionShape2D.shape.extents.x
 
 func draw_grip_range():
 	if not grip_vectors.empty():
@@ -116,3 +124,9 @@ func draw_grip_range():
 			elif surface.hit:
 				color = Color(0, 0, 1)
 			draw_line(Vector2(0, 0) + grip_vector, grip_vector + range_value, color, 1)
+
+func in_contact_range(distance):
+	return distance <= THRESHOLD
+
+func reset_rotation():
+	rotation = 0
